6ebbd60e5597022a33c09d9b9e72702e319cf561
pritpalkaur <pritpalkaur05@gmail.com>
2026-01-14 16:50:59 +0800

    Add Angular Material UI

--- AngularApp/Product-App/src/app/product-component/product-component.ts
+++ AngularApp/Product-App/src/app/product-component/product-component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
-import { HttpClient,HttpClientModule } from '@angular/common/http';
-import { Product } from '../models/product';
-import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [
-    HttpClientModule,
+    CommonModule,
     FormsModule,
-    CommonModule
-  ],   // <-- Add this
+    HttpClientModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './product-component.html',
   styleUrl: './product-component.css',
 })
-
 export class ProductComponent {
-
+  displayedColumns: string[] = ['id', 'name', 'price', 'actions'];
   products: Product[] = [];
   newProduct: Product = { id: 0, name: '', price: 0 };
 
@@ -41,17 +52,63 @@ export class ProductComponent {
     this.newProduct = { ...product };
     this.deleteProduct(product.id);
   }
-} 
+}
+
 
 // import { Component } from '@angular/core';
+// import { HttpClient,HttpClientModule } from '@angular/common/http';
+// import { Product } from '../models/product';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
 
 // @Component({
-//   selector: 'app-product-component',
-//    standalone: true,
-//   imports: [],
+//   selector: 'app-product',
+//   standalone: true,
+//   imports: [
+//     HttpClientModule,
+//     FormsModule,
+//     CommonModule
+//   ],   // <-- Add this
 //   templateUrl: './product-component.html',
 //   styleUrl: './product-component.css',
 // })
+
 // export class ProductComponent {
 
-// }
+//   products: Product[] = [];
+//   newProduct: Product = { id: 0, name: '', price: 0 };
+
+//   constructor(private http: HttpClient) {}
+
+//   ngOnInit() {
+//     this.http.get<Product[]>('assets/products.json')
+//       .subscribe(data => this.products = data);
+//   }
+
+//   addProduct() {
+//     this.products.push({ ...this.newProduct });
+//     this.newProduct = { id: 0, name: '', price: 0 };
+//   }
+
+//   deleteProduct(id: number) {
+//     this.products = this.products.filter(p => p.id !== id);
+//   }
+
+//   editProduct(product: Product) {
+//     this.newProduct = { ...product };
+//     this.deleteProduct(product.id);
+//   }
+// } 
+
+// // import { Component } from '@angular/core';
+
+// // @Component({
+// //   selector: 'app-product-component',
+// //    standalone: true,
+// //   imports: [],
+// //   templateUrl: './product-component.html',
+// //   styleUrl: './product-component.css',
+// // })
+// // export class ProductComponent {
+
+// // }
